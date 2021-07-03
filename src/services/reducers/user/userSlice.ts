ae139478e05683a08980918870e221b88c377916
alex-kurkov <alexkourkov@yandex.ru>
2021-07-03 14:59:42 +0500

    finish TSing userSlice

--- src/services/reducers/user/userSlice.ts
+++ src/services/reducers/user/userSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IOrder } from '../../../types';
 
 interface TUserState {
@@ -37,7 +37,7 @@ export const userSlice = createSlice({
       state.passwordReset = true;
       state.passwordResetSuccess = false;
     },
-    setUser: (state, action) => {
+    setUser: (state, action: PayloadAction<{name: string, email: string}>) => {
       state.loggedIn = true;
       state.name = action.payload.name;
       state.email = action.payload.email;
@@ -56,7 +56,7 @@ export const userSlice = createSlice({
     setAuthSocketError: (state) => {
       state.socketConnected = false;
     },
-    getAuthSocketMessage: (state, action) => {
+    getAuthSocketMessage: (state, action: PayloadAction<{orders: [IOrder]; total: number, totalToday: number}>) => {
       state.userOrders = action.payload.orders;
       state.total = action.payload.total;
       state.totalToday = action.payload.totalToday;
