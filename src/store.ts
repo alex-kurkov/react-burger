7eeee7002efe7d7dca43434a40a3483fe73d9f76
alex-kurkov <alexkourkov@yandex.ru>
2021-07-04 22:12:24 +0500

    fixed thunk types

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
-import { Action, ActionCreator } from 'redux';
+import { Action, ActionCreator, AnyAction } from 'redux';
 import { socketMiddleware } from './services/middleware/index';
 import { SOCKET_ORDERS_URL, SOCKET_ORDERS_URL_WITH_AUTH } from './utils/constants';
 import { wsActions, wsActionsAuth } from './services/actions/ws';
@@ -16,5 +16,10 @@ const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export type AppThunk<DATA = any> = ActionCreator<ThunkAction<Promise<any> | Action<any>, RootState, DATA, Action>>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AnyAction
+>
 export default store;
